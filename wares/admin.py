e70823e39e468ced913f98a4638aa0f2e82f7b91
Victor Gridasov <vgridasov@gmail.com>
2018-08-09 09:42:24 +0300

    Добавлено управление дополнительными полями в админке

--- wares/admin.py
+++ wares/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Ware, Seller, Price
+from .models import Cat, Vendor, Ware, Seller, Price
 
+admin.site.register(Cat)
+admin.site.register(Vendor)
 admin.site.register(Ware)
 admin.site.register(Seller)
 admin.site.register(Price)
